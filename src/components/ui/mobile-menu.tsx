0bc15d13d43d88f40383c256cb76088802fba7e2
jan_jirek <j.jirek@quadient.com>
2025-12-05 10:36:53 +0100

    clanek

--- src/components/ui/mobile-menu.tsx
+++ src/components/ui/mobile-menu.tsx
@@ -124,7 +124,7 @@ const sluzbyItems: DropdownItem[] = [
                 className="flex rounded-lg px-2 py-1.5 text-gray-700 hover:bg-gray-100"
                 onClick={() => setMobileNavOpen(false)}
               >
-                Nemovitosti
+                Nabídka Nemovitostí
               </a>
             </li>
             <li>
